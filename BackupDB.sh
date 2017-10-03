f8b63d2e972e852f422af219c34c48c0627e7704
bayuadinh <adin@daiserver.com>
2017-10-04 06:29:49 +0700

    Update Database Backup

---
+++ BackupDB.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Masukkan Username Database : "
+read UserDB
+echo "Masukkan Password Database: "
+read PassDB
+echo "Masukkan Nama Database: "
+read NameDB
+echo "Masukkan Output file Database: "
+read PathDB
+
+//Memberi nama Database
+namafiledb = $PathDB-$(date +%Y%m%d).sql
+
+//Menghapus DB yang lebih dari 7 hari
+hapusfiledb = $PathDB-$(date +%Y%m%d --date="7 hari yang lalu").sql
+cd $PathDB
+rm -rf $namafiledb
+
+//Melakukan proses backup
+echo "Backup database terakhir"
+mysqldump --opt -u $UserDB --password="${PassDB}" $NameDB > $namafiledb
+echo "Backup Selesai..."
+
+//Commit ke Gitlab
+git add .
+echo "commit ke remote git"
+git commit -am "Database Update"
+git push -u origin master
+echo "Menghapus Database Minggu terakhir"
+rm -rf $hapusfiledb
